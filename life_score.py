74a27d6688216daf60811830606373af612b1f65
xzcvczcx <147779163+xzcvczcx@users.noreply.github.com>
2023-11-01 23:58:03 +0000

    Once user reaches 21 rounds break out of loop and end the game

--- life_score.py
+++ life_score.py
@@ -4,8 +4,9 @@ round = 0
 
 score = 2
 
+
 # Number checker function
-def num_check (question):
+def num_check(question):
   error = "Please enter a whole number"
 
   valid = False
@@ -18,24 +19,29 @@ def num_check (question):
     except ValueError:
       print(error)
 
+
 # Game loop + Score count
 while True:
   round += 1
   print("Round {}".format(round))
-  num1 = randint(1,10)
-  num2 = randint(1,10)
+  num1 = randint(1, 10)
+  num2 = randint(1, 10)
   product = num1 * num2
   response = num_check(f'What is {num1} x {num2}? = ')
   if response == product:
     print("Correct!")
   else:
     print("Incorrect..")
-    score -=1
+    score -= 1
   print("Life score: {} \n".format(score))
 
-# End of game when run out of lives
+  # End of game when run out of lives
   if score == 0:
-    print("you dead L try again never back down never what?""")
+    print("you dead L try again never back down never what?"
+          "")
     break
-    
-print("well done you got {} rounds".format(round))
+  if round == 21:
+    print("Congratulations you have completed the quiz")
+    break
+print("you reached {} rounds".format(round))
+  # Once user reaches round 21 break out of loop
